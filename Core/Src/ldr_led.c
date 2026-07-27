bba95291468b1da3e373f472205c54c4b9d353d0
heeyeon5877-tech <heeyeon5877@gmail.com>
2026-07-27 11:22:03 +0900

    센서값 평균 5회로 변경

--- Core/Src/ldr_led.c
+++ Core/Src/ldr_led.c
@@ -3,21 +3,28 @@
 uint32_t average_adc_value = 0;
 
 void Ldr_Led_Update(void) {
+    uint32_t sum = 0;
+    
     // ADC 값을 읽기 시작합니다.
     HAL_ADC_Start(&hadc1);
 
-    // ADC 변환이 완료될 때까지 기다립니다.
-    HAL_ADC_PollForConversion(&hadc1, 10);
-    uint32_t adc_value = HAL_ADC_GetValue(&hadc1);
+    for(int i =0; i<5; i++)
+    {
+        HAL_ADC_PollForConversion(&hadc1, 10);
+        sum += HAL_ADC_GetValue(&hadc1);
+    }
+    // // ADC 변환이 완료될 때까지 기다립니다.
+    // HAL_ADC_PollForConversion(&hadc1, 10);
+    // uint32_t adc_value = HAL_ADC_GetValue(&hadc1);
 
-    // 두 번째 ADC 값을 읽습니다.
-    HAL_ADC_PollForConversion(&hadc1, 10);
-    uint32_t adc_value2 = HAL_ADC_GetValue(&hadc1);
+    // // 두 번째 ADC 값을 읽습니다.
+    // HAL_ADC_PollForConversion(&hadc1, 10);
+    // uint32_t adc_value2 = HAL_ADC_GetValue(&hadc1);
 
     HAL_ADC_Stop(&hadc1);
 
-    // 두 ADC 값의 평균을 계산합니다.
-    average_adc_value = (adc_value + adc_value2) / 2;
+    //ADC 값의 평균을 계산합니다.
+    average_adc_value = sum / 5;
 
     uint32_t inverted = 4095 - average_adc_value; // LDR 값에 따라 반전된 값을 계산합니다.
     // Counter Period를 999로 설정함. pwm값으로 스케일링
